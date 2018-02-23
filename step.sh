d3740ab1566e271e5dec5f31425006a862f0fa60
kawaiseb <spousse@pagesjaunes.fr>
2018-02-23 10:20:27 +0100

    feat(sh):preparing dependencies and launching script with parameters

--- step.sh
+++ step.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 set -ex
 
-echo $BLOP_BLOP
-echo "This is the value specified for the input 'example_step_input': ${example_step_input}"
+THIS_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo "***********"
+echo "* TWITTER *"
+echo "***********"
+echo "Installing NPM dependencies..."
+npm install --prefix $THIS_SCRIPT_DIR twitter --save
 
-#
-# --- Export Environment Variables for other Steps:
-# You can export Environment Variables for other Steps with
-#  envman, which is automatically installed by `bitrise setup`.
-# A very simple example:
-envman add --key EXAMPLE_STEP_OUTPUT --value 'the value you want to share'
-# Envman can handle piped inputs, which is useful if the text you want to
-# share is complex and you don't want to deal with proper bash escaping:
-#  cat file_with_complex_input | envman add --KEY EXAMPLE_STEP_OUTPUT
-# You can find more usage examples on envman's GitHub page
-#  at: https://github.com/bitrise-io/envman
+echo "Launching step script to post status on twitter..."
+
+$THIS_SCRIPT_DIR/twitter.js "${wtu_debug_mode}" "${consumer_key}" "${consumer_secret}" "${access_token_secret}" "${access_token_key}" "${screen_name}" "${status}"
+
+exit 0;
+
+# If error exit 1
+
 
 #
 # --- Exit codes:
